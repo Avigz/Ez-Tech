65ac6bc04b0472716340bfbf44c43dc4ebc6ce33
Saad Jalawan <saad0355@edu.easj.dk>
2019-11-21 12:39:25 +0100

    tilføjet sider

--- Ez-Tech/View/Admin/AdminPage.xaml.cs
+++ Ez-Tech/View/Admin/AdminPage.xaml.cs
@@ -15,18 +15,21 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Client.View
+namespace Ez_Tech.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class HjælperPage : Page
+    public sealed partial class BlankPage2 : Page
     {
-        public HjælperPage()
+        public BlankPage2()
         {
             this.InitializeComponent();
         }
 
-      
+        private void TextBlock_SelectionChanged()
+        {
+
+        }
     }
 }
