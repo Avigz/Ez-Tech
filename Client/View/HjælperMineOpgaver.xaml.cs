bcef7be34c1f3b66f2761ffcbd9b8d88010dc508
Mathias Elmhauge Petersen <math955e@edu.easj.dk>
2019-11-22 10:18:26 +0100

    opdateret click funktioner på hjælpermineopgaver

--- Client/View/HjælperMineOpgaver.xaml.cs
+++ Client/View/HjælperMineOpgaver.xaml.cs
@@ -26,5 +26,47 @@ namespace Client.View
         {
             this.InitializeComponent();
         }
+
+       
+
+        private void MenuButton3_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MenuButton4_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MenuButton5_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TextBlock_OnSelectionChanged(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MenuButton6_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MenuButton7_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void MenuButton8_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void HamburgerButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
