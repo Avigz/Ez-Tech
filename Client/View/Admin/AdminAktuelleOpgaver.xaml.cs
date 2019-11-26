9b262859b756bbfeb6410096dd7d03c8b095c498
Solodanser1809 <futhark1536@gmail.com>
2019-11-26 18:39:41 +0100

    Genopsætning af AdminFærdigeOpgaver-side

--- Client/View/Admin/AdminAktuelleOpgaver.xaml.cs
+++ Client/View/Admin/AdminAktuelleOpgaver.xaml.cs
@@ -20,9 +20,9 @@ namespace Client.View.Admin
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class AktuelleOpgaver : Page
+    public sealed partial class AdminAktuelleOpgaver : Page
     {
-        public AktuelleOpgaver()
+        public AdminAktuelleOpgaver()
         {
             this.InitializeComponent();
         }
