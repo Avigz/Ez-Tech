3907dfa5f64797477807d3b4002a9379f2f47285
Saad Jalawan <saad0355@edu.easj.dk>
2020-01-02 12:27:16 +0100

    Admin aktuelle opgaver finpudsning.

--- Client/Model/IWebApiAsync.cs
+++ Client/Model/IWebApiAsync.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 
 namespace Client.Model
 {
+
+
         public interface IWebAPIAsync<T>
         {
-            Task<List<T>> Load();
+           Task<List<T>> Load();
             Task Create(int key, T obj);
             Task<T> Read(int key);
             Task Update(int key, T obj);
